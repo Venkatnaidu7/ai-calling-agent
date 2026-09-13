438308bdd16e370627f71fbbf27719305ff330fd
venkat <233468504+Venkatnaidu7@users.noreply.github.com>
2026-09-13 14:39:52 +0530

    fix: use secret ARNs for ECS production secrets

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -11,14 +11,14 @@ variable "database_name" { type = string default = "voice" }
 variable "database_username" { type = string default = "voice" }
 variable "database_password" { type = string sensitive = true }
 variable "redis_auth_token" { type = string sensitive = true }
-variable "secret_key" { type = string sensitive = true }
-variable "openai_api_key" { type = string sensitive = true }
-variable "twilio_account_sid" { type = string sensitive = true default = "" }
-variable "twilio_auth_token" { type = string sensitive = true default = "" }
-variable "plivo_auth_id" { type = string sensitive = true default = "" }
-variable "plivo_auth_token" { type = string sensitive = true default = "" }
-variable "stripe_secret_key" { type = string sensitive = true }
-variable "stripe_webhook_secret" { type = string sensitive = true }
+variable "secret_key_arn" { type = string }
+variable "openai_api_key_arn" { type = string }
+variable "twilio_account_sid_arn" { type = string default = "" }
+variable "twilio_auth_token_arn" { type = string default = "" }
+variable "plivo_auth_id_arn" { type = string default = "" }
+variable "plivo_auth_token_arn" { type = string default = "" }
+variable "stripe_secret_key_arn" { type = string }
+variable "stripe_webhook_secret_arn" { type = string }
 variable "public_base_url" { type = string }
 variable "frontend_url" { type = string }
 variable "api_cpu" { type = number default = 512 }
