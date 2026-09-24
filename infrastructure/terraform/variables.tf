00d19d85be7d76603516aa58f923517bab80744d
venkat <233468504+Venkatnaidu7@users.noreply.github.com>
2026-09-24 12:20:57 +0530

    infra: make DigitalOcean the active Terraform target

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,2 +1,7 @@
-variable "aws_region" { type=string default="ap-south-1" }
-variable "environment" { type=string default="staging" }
+variable "do_token" { type = string sensitive = true }
+variable "environment" { type = string default = "staging" }
+variable "region" { type = string default = "blr1" }
+variable "droplet_size" { type = string default = "s-2vcpu-4gb" }
+variable "ssh_key_ids" { type = list(string) }
+variable "allowed_ssh_cidrs" { type = list(string) default = [] }
+variable "domain" { type = string default = "" }
